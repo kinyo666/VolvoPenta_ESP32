08f108e7b3a1e80d54e8164f3b82c224c3517ef8
kinyo666 <kinyo666@yahoo.fr>
2024-11-12 16:26:34 +0100

    v1.0.2 - Add Filter for coolant temperature in K

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,31 @@
 /*
   TODO :
   P0
-  - Mettre des valeurs à 0.0 quand le moteur ne tourne pas ? (CoolantTemperature avec K <= 273.15)
+  - Utiliser un MovingAverage sur la valeur RPM
   
   P1
   - Ajouter une moyenne de consommation de carburant réelle
 
   P2
   - Ajouter un compteur de chaine (à partir du signal 12V?)
+  - Surcharger la classe SKMetaData pour envoyer les données de zones
 
   @author kinyo666
-  @version 1.0.0
-  @date 03/11/2024
+  @version 1.0.2
+  @date 12/11/2024
   @link SensESP Documentation : https://signalk.org/SensESP/
-  @link Based on https://github.com/Techstyleuk/SensESP_3_Battery_Monitor
+  @link Based on https://github.com/Boatingwiththebaileys/ESP32-code
   @link https://www.arduino.cc/reference/en/libraries/ina3221/
   @link https://signalk.org/specification/1.7.0/schemas/definitions.json
   */
 
 //#include <Arduino.h>
 //#include <WiFi.h>
-#include <Wire.h>
+//#include <Wire.h>
 #include <INA3221.h>
 //#include <OneWire.h>
 #include <sensesp.h>
-//#include "sensesp_app.h"
 #include <sensesp_app_builder.h>
-#include <sensesp/sensors/sensor.h>
 #include <sensesp/sensors/constant_sensor.h>
 #include <sensesp/sensors/digital_input.h>
 //#include "sensesp/sensors/analog_input.h"
@@ -51,8 +50,8 @@
 #define INA3221_BABORD_0 0
 #define INA3221_CUVES_1 1
 #define INA3221_TRIBORD_2 2
-//#define INA3221_TRIBORD_3 3
-#define INA3221_NB 3
+#define INA3221_TRIBORD_3 3
+#define INA3221_NB 3      // 4
 
 // RPM sensors PC817
 #define PC817_BABORD_PIN 34
@@ -64,7 +63,9 @@
 #define ENGINE_NB 2
 #define ENGINE_SK_PATH_NB 3
 
-//#define DEBUG_MODE 1
+// Use Debug Mode for verbose logs and Fake Mode to simulate RPM data
+#define DEBUG_MODE 1
+//#define FAKE_MODE 1
 
 using namespace sensesp;
 using namespace sensesp::onewire;
@@ -95,16 +96,15 @@ const String conf_path_volt[INA3221_NB][INA3221_CH_NUM] = {
         {"/CONFIG/BABORD/INA3221/0/LINEAR_CH1",   "/CONFIG/BABORD/INA3221/0/LINEAR_CH2",  "/CONFIG/BABORD/INA3221/0/LINEAR_CH3"},
         {"/CONFIG/CUVES/INA3221/1/LINEAR_CH1",    "/CONFIG/CUVES/INA3221/1/LINEAR_CH2",   "/CONFIG/CUVES/INA3221/1/LINEAR_CH3"},
         {"/CONFIG/TRIBORD/INA3221/2/LINEAR_CH1",  "/CONFIG/TRIBORD/INA3221/2/LINEAR_CH2", "/CONFIG/TRIBORD/INA3221/2/LINEAR_CH3"}};
-const String conf_path_engines[ENGINE_NB] = {"/CONFIG/BABORD/EL817/FREQUENCY_RPM", 
-                                            "/CONFIG/TRIBORD/EL817/FREQUENCY_RPM"};
-
-const unsigned int read_delay = 2000;
+const String conf_path_engines[ENGINE_NB] = {"/CONFIG/BABORD/PC817/FREQUENCY_RPM", 
+                                            "/CONFIG/TRIBORD/PC817/FREQUENCY_RPM"};
+const unsigned int read_delay = 1000;
 
 // Temperature & Voltage sensors
 OneWireTemperature* sensor_temp[DS18B20_NB];                  // 3 DS18B20 Temperature values
 INA3221 *sensor_INA3221[INA3221_NB];                          // 4 INA3221 Voltage Sensors with 3 channels each
 RepeatSensor<float> *sensor_volt[INA3221_NB][INA3221_CH_NUM]; // 4 Voltage values for each channel
-#ifndef DEBUG_MODE
+#ifndef FAKE_MODE
 DigitalInputCounter *sensor_engine[ENGINE_NB];                // 2 PC817 RPM Sensors values
 #else
 FloatConstantSensor *sensor_engine[ENGINE_NB];                // Fake values
@@ -196,21 +196,30 @@ float (*LinearPositive::function_)(float, float, float) =
         return (0.0f);
     };
 
-const ParamInfo LinearPositive::param_info_[] = {{"multiplier", "Multiplier"}, {"offset", "Constant offset"}};
+const ParamInfo LinearPositive::param_info_[] = {{"multiplier", "Multiplier"}, {"offset", "Offset"}};
 
-// Define the engines transform to output data to Signal-K in m3/s
+// Filter Kelvin values out of range
+auto filterKelvinValues = [](float input, float offset = 273.15, float max = 413.1) -> float {
+    return ((((input - offset) > 0) && (input < max)) ? input : offset);
+};
+
+const ParamInfo* filterKelvinValues_ParamInfo = new ParamInfo[2]{{"offset", "Offset"}, {"max", "Max value"}};
+
+// Define the engines transform to output data to Signal-K
 class EngineDataTransform {
   public :
     Frequency *freq;
     Linear *hz_to_rpm;
     FuelConsumption *rpm_to_lhr;
     LinearPositive *lhr_to_m3s;
+    MovingAverage *moving_avg;
 
     EngineDataTransform(float multiplier, uint8_t engineID) { 
         freq = new Frequency(multiplier, conf_path_engines[engineID]);
-        hz_to_rpm = new Linear(59.999999999999, 0.0);                   // Hertz (Hz) to Revolutions Per Minute (RPM)
+        hz_to_rpm = new Linear(60, 0.0);                                // Hertz (Hz) to Revolutions Per Minute (RPM)
         rpm_to_lhr = new FuelConsumption();                             // RPM to Liter per Hour (l/hr)
         lhr_to_m3s = new LinearPositive(1 / (3.6 * pow(10, 6)), 0.0);   // Liter per Hour (l/hr) to Meter cube per second (m3/s)
+        moving_avg = new MovingAverage(2, 1.0);                         // Moving average with 2 samples
       }
 };
 
@@ -224,7 +233,7 @@ void setupSensorINA3221(u_int8_t index, ina3221_addr_t addr = INA3221_ADDR40_GND
   sensor_INA3221[index] = new INA3221(addr);  // Set I2C default address to 0x40 GND or addr
   sensor_INA3221[index]->begin(&Wire);        // Default shunt resistors = 10 mOhm (R100)
   sensor_INA3221[index]->reset();
-  delay(10);                                  // Wait for the next sensor setup
+  delay(10);                                  // Wait 10 ms for the next sensor setup
   #ifdef DEBUG_MODE
   Serial.printf("Setup Sensor INA3221 0x%X completed : ManufID = %i ; DieID = %i\n", addr, sensor_INA3221[index]->getManufID(), sensor_INA3221[index]->getDieID());
   #endif
@@ -311,11 +320,15 @@ void setupVoltageSensors() {
 
     @link https://github.com/SignalK/SensESP/blob/main/examples/temperature_sender.cpp
   */
+  
+  // Input values for VoltageDivider ; INA3221 is 0.1 ohm (R100) and fuel/water sensors are ~180 ohms max
+  const float Vin = 3.32; // 3.5
+  const float R1 = 1.0; // 120
+
   setupSensorINA3221(INA3221_BABORD_0, INA3221_ADDR40_GND);
-  //setupSensorINA3221(INA3221_CUVES_1, INA3221_ADDR41_VCC);
   setupSensorINA3221(INA3221_CUVES_1, INA3221_ADDR42_SDA);
-  //setupSensorINA3221(INA3221_TRIBORD_2, INA3221_ADDR42_SDA);
-  //setupSensorINA3221(INA3221_OTHERS_3, INA3221_ADDR43_SCL);
+  //setupSensorINA3221(INA3221_TRIBORD_2, INA3221_ADDR43_SCL);
+  //setupSensorINA3221(INA3221_OTHERS_3, INA3221_ADDR41_VCC);
 
   sensor_volt[INA3221_BABORD_0][INA3221_CH1] = new RepeatSensor<float>(read_delay, getVoltageINA3221_BABORD0_CH1);
   sensor_volt[INA3221_BABORD_0][INA3221_CH2] = new RepeatSensor<float>(read_delay, getVoltageINA3221_BABORD0_CH2);
@@ -330,18 +343,20 @@ void setupVoltageSensors() {
   //sensor_volt[INA3221_TRIBORD_2][INA3221_CH3] = new RepeatSensor<float>(read_delay, getVoltageINA3221_TRIBORD2_CH3);
     
   // Voltage for Coolant Temperature gauge
-  const float Vin = 3.3; // 3.5
-  const float R1 = 1.0; // 120
   sensor_volt[INA3221_BABORD_0][INA3221_CH1]
     ->connect_to(new VoltageDividerR2(R1, Vin, conf_path_volt[INA3221_BABORD_0][INA3221_CH1] + "/VOLTAGE_DIVIDER"))
     ->connect_to(new CoolantTemperature(conf_path_volt[INA3221_BABORD_0][INA3221_CH1] + "/COOLANT_TEMPERATURE"))
+    ->connect_to(new LambdaTransform<float, float, float, float>(filterKelvinValues, 273.15, 413.1, filterKelvinValues_ParamInfo, 
+                                                                conf_path_volt[INA3221_BABORD_0][INA3221_CH1] + "/TRANSFORM"))
     ->connect_to(new LinearPositive(1.0, 0.0, conf_path_volt[INA3221_BABORD_0][INA3221_CH1] + "/LINEAR_POSITIVE"))
     //->connect_to(new MovingAverage(4, 1.0, conf_path_volt[INA3221_BABORD_0][INA3221_CH1] + "/MOVING_AVERAGE"))
     ->connect_to(new SKOutputFloat(sk_path_volt[INA3221_BABORD_0][INA3221_CH1],
                     new SKMetadata("K", "T° Eau Babord", "Coolant temperature", "Temp Babord")));
 
+  #ifdef DEBUG_MODE
   sensor_volt[INA3221_BABORD_0][INA3221_CH1]
     ->connect_to(new SKOutputFloat(sk_path_volt[INA3221_BABORD_0][INA3221_CH1] + ".raw"));
+  #endif
 
   // Voltage for Oil Pressure gauge
   sensor_volt[INA3221_BABORD_0][INA3221_CH2]
@@ -352,8 +367,10 @@ void setupVoltageSensors() {
     ->connect_to(new SKOutputFloat(sk_path_volt[INA3221_BABORD_0][INA3221_CH2],
                     new SKMetadata("Pa", "Pression Huile Babord", "Oil pressure", "Huile Babord")));
 
+  #ifdef DEBUG_MODE
   sensor_volt[INA3221_BABORD_0][INA3221_CH2]
     ->connect_to(new SKOutputFloat(sk_path_volt[INA3221_BABORD_0][INA3221_CH2] + ".raw"));
+  #endif
 
   // Voltage for Volts gauge
   sensor_volt[INA3221_BABORD_0][INA3221_CH3]
@@ -375,22 +392,46 @@ void setupVoltageSensors() {
   
   // 1. Take the maximum analog input value (i.e. value when sensor is at the high end)
   // 2. Divide 1 by max value. In my case: 1 / 870 = 0.001149425
+  // Assuming the resistance is 3-180 ohms for VDO / Veritron gauge
 
   // Fuel tank volume on portside
   sensor_volt[INA3221_CUVES_1][INA3221_CH1]
+    ->connect_to(new VoltageDividerR2(R1, Vin, conf_path_volt[INA3221_CUVES_1][INA3221_CH1] + "/VOLTAGE_DIVIDER"))
+    ->connect_to(new LinearPositive(1.0, 0.0, conf_path_volt[INA3221_CUVES_1][INA3221_CH1] + "/LINEAR_POSITIVE"))
     ->connect_to(new MovingAverage(4, 1.0, conf_path_volt[INA3221_CUVES_1][INA3221_CH1] + "/MOVING_AVERAGE"))
     ->connect_to(new SKOutputFloat(sk_path_volt[INA3221_CUVES_1][INA3221_CH1],
                   new SKMetadata("m3", "Cuve Babord", "Niveau carburant Babord", "Cuve Babord")));
+
+  #ifdef DEBUG_MODE
+  sensor_volt[INA3221_CUVES_1][INA3221_CH1]
+    ->connect_to(new SKOutputFloat(sk_path_volt[INA3221_CUVES_1][INA3221_CH1] + ".raw"));
+  #endif
+
   // Fuel tank volume on starboard
   sensor_volt[INA3221_CUVES_1][INA3221_CH2]
+    ->connect_to(new VoltageDividerR2(R1, Vin, conf_path_volt[INA3221_CUVES_1][INA3221_CH2] + "/VOLTAGE_DIVIDER"))
+    ->connect_to(new LinearPositive(1.0, 0.0, conf_path_volt[INA3221_CUVES_1][INA3221_CH2] + "/LINEAR_POSITIVE"))
     ->connect_to(new MovingAverage(4, 1.0, conf_path_volt[INA3221_CUVES_1][INA3221_CH2] + "/MOVING_AVERAGE"))
     ->connect_to(new SKOutputFloat(sk_path_volt[INA3221_CUVES_1][INA3221_CH2],
                   new SKMetadata("m3", "Cuve Tribord", "Niveau carburant Tribord", "Cuve Tribord")));
+
+  #ifdef DEBUG_MODE
+  sensor_volt[INA3221_CUVES_1][INA3221_CH2]
+    ->connect_to(new SKOutputFloat(sk_path_volt[INA3221_CUVES_1][INA3221_CH2] + ".raw"));
+  #endif
+
   // Water tank volume
   sensor_volt[INA3221_CUVES_1][INA3221_CH3]
+    ->connect_to(new VoltageDividerR2(R1, Vin, conf_path_volt[INA3221_CUVES_1][INA3221_CH3] + "/VOLTAGE_DIVIDER"))
+    ->connect_to(new LinearPositive(1.0, 0.0, conf_path_volt[INA3221_CUVES_1][INA3221_CH3] + "/LINEAR_POSITIVE"))
     ->connect_to(new MovingAverage(4, 1.0, conf_path_volt[INA3221_CUVES_1][INA3221_CH3] + "/MOVING_AVERAGE"))
     ->connect_to(new SKOutputFloat(sk_path_volt[INA3221_CUVES_1][INA3221_CH3],
                   new SKMetadata("m3", "Cuve Eau douce", "Niveau eau douce", "Cuve Eau")));
+
+  #ifdef DEBUG_MODE
+  sensor_volt[INA3221_CUVES_1][INA3221_CH3]
+    ->connect_to(new SKOutputFloat(sk_path_volt[INA3221_CUVES_1][INA3221_CH3] + ".raw"));
+  #endif
 }
 
 /* https://signalk.org/specification/1.7.0/doc/data_model_metadata.html
@@ -469,6 +510,13 @@ SKMetadata getEnginesSKMetadata() {
 }
 
 // Measure Engines RPM
+/*
+  multiplier_port and multiplier_starboard needs to be adjusted based on flywheel to Alternator W ratio.
+  Example: 1 turn on an AQAD41 makes 2.45 turns on the Alternator, and the Alternator has 6 poles which makes a total of 2.45 x 6 = 14.7 Hz per turn. SignalK needs info in Hz so we need to divide the incoming value with 14.7, or as in our case multiply with (1/14.7) = 0,06803
+  If Ratio is unknown, the original Tachometer might have a max Impulses/min written on it, divide that with max rpm on the meter and you'll get the ratio. Tachometer 860420 is marked with 73500Imp/min and has a scale to 5000rpm. 73500 divided with 5000 equals 14,7, Tada!
+  If above multiplier needs to be recalculated, the FuelMultipliers needs to be recalculated as well, they're both based on AQAD41 values right now.
+  AQAD41 example: 60 divided with FlyWheel To W Ratio (60 / 14,7 = 4,08)
+*/
 void setupRPMSensors() {
   // Step 1 : calibrate the multiplier with known values (CurveInterpolator class) from analog gauge
   //const float multiplier = 1.0 / 97.0;
@@ -478,10 +526,10 @@ void setupRPMSensors() {
   EngineDataTransform *engine_babord = new EngineDataTransform(multiplier, ENGINE_BABORD);
   EngineDataTransform *engine_tribord = new EngineDataTransform(multiplier, ENGINE_TRIBORD);
 
-  // Step 2 : instanciate DigitalInputCounter from PC817 sensor
-  #ifndef DEBUG_MODE
-  sensor_engine[ENGINE_BABORD] = new DigitalInputCounter(PC817_BABORD_PIN, INPUT, RISING, read_rpm_delay);    // INPUT or INPUT_PULLUP ? (depends on pin)
-  sensor_engine[ENGINE_TRIBORD] = new DigitalInputCounter(PC817_TRIBORD_PIN, INPUT, RISING, read_rpm_delay);  // INPUT or INPUT_PULLUP ? (depends on pin)
+  // Step 2 : instanciate DigitalInputCounter to read PC817 sensor values
+  #ifndef FAKE_MODE
+  sensor_engine[ENGINE_BABORD] = new DigitalInputCounter(PC817_BABORD_PIN, INPUT, RISING, read_rpm_delay);    // INPUT for GPIO 34-35 or INPUT_PULLUP for others pins
+  sensor_engine[ENGINE_TRIBORD] = new DigitalInputCounter(PC817_TRIBORD_PIN, INPUT, RISING, read_rpm_delay);  // INPUT for GPIO 34-35 or INPUT_PULLUP for others pins
   #else
   sensor_engine[ENGINE_BABORD] = new FloatConstantSensor(50.0, 1, "/config/BABORD/EL817/CONSTANT");   // Fake sensor for test purpose
   sensor_engine[ENGINE_TRIBORD] = new FloatConstantSensor(60.0, 1, "/config/TRIBORD/EL817/CONSTANT"); // Fake sensor for test purpose
@@ -490,15 +538,17 @@ void setupRPMSensors() {
   // Step 3 : connect the output of sensor to the input of Frequency() and the Signal K output as a float in Hz
   sensor_engine[ENGINE_BABORD]
     ->connect_to(engine_babord->freq)
+    //->connect_to(engine_babord->moving_avg)
     ->connect_to(new SKOutputFloat(sk_path_engines[ENGINE_BABORD][REVOLUTIONS], 
                   new SKMetadata("Hz", "Moteur Babord", "Engine revolutions (x60 for RPM)", "RPM")));
 
   sensor_engine[ENGINE_TRIBORD]
     ->connect_to(engine_tribord->freq)
+    //->connect_to(engine_tribord->moving_avg)
     ->connect_to(new SKOutputFloat(sk_path_engines[ENGINE_TRIBORD][REVOLUTIONS],
                   new SKMetadata("Hz", "Moteur Tribord", "Engine revolutions (x60 for RPM)", "RPM")));
 
-// Step 4 : transforms the output of Frequency to RPM and Liter per hour from FuelConsomption - cf time_counter.cpp
+  // Step 4 : transforms the output of Frequency to RPM and Liter per hour from FuelConsomption
   engine_babord->freq
     ->connect_to(engine_babord->hz_to_rpm)
     ->connect_to(engine_babord->rpm_to_lhr)
@@ -513,16 +563,16 @@ void setupRPMSensors() {
     ->connect_to(new SKOutputFloat(sk_path_engines[ENGINE_TRIBORD][FUELRATE],
                   new SKMetadata("m3/s", "Conso Tribord", "Fuel rate of consumption", "L/hr")));
 
-// Step 5 : change the state if needed
+  // Step 5 : change the engine state if needed
 }
 
 // The setup function performs one-time application initialization.
 void setup() {
-  // Some initialization boilerplate when in debug mode...
-  //#ifndef SERIAL_DEBUG_DISABLED
-  //  SetupSerialDebug(115200);
-  //#endif  
-  SetupLogging(ESP_LOG_DEBUG); //ESP_LOG_INFO
+  #ifdef DEBUG_MODE
+  SetupLogging(ESP_LOG_DEBUG);
+  #else
+  SetupLogging(ESP_LOG_INFO);
+  #endif
   Serial.begin(115200);
 
   // Create the global SensESPApp() object.
@@ -532,9 +582,7 @@ void setup() {
 //                ->set_sk_server(signalkip, signalkport)
 //                  ->set_wifi(ssid, password)
 
-  //setupSignalKPath();
-  //setupSensESPConfig();
-  setupTemperatureSensors();
+  //setupTemperatureSensors();
   setupVoltageSensors();
   setupRPMSensors();
 }
@@ -544,6 +592,4 @@ void setup() {
 void loop() { 
   static auto event_loop = SensESPBaseApp::get_event_loop();
   event_loop->tick();
-
-  delay(2000);
 }
